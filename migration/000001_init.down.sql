a627ac3ecc864b2bdc945814e71fe0941a77e07d
emeka-chukwu <emekapascal11@gmail.com>
2023-12-01 16:43:11 +0100

    init with its content

---
+++ migration/000001_init.down.sql
@@ -0,0 +1,29 @@
+-- Drop foreign key constraints
+
+-- Drop constraints in transactions table
+ALTER TABLE "transactions" DROP CONSTRAINT IF EXISTS "transactions_user_id_fkey";
+
+-- Drop constraints in sessions table
+ALTER TABLE "sessions" DROP CONSTRAINT IF EXISTS "sessions_user_id_fkey";
+
+-- Drop constraints in verifications table
+ALTER TABLE "verifications" DROP CONSTRAINT IF EXISTS "verifications_user_id_fkey";
+
+-- Drop constraints in payment_pins table
+ALTER TABLE "payment_pins" DROP CONSTRAINT IF EXISTS "payment_pins_user_id_fkey";
+
+-- Drop constraints in scheduled_payments table
+ALTER TABLE "scheduled_payments" DROP CONSTRAINT IF EXISTS "scheduled_payments_user_id_fkey";
+ALTER TABLE "scheduled_payments" DROP CONSTRAINT IF EXISTS "scheduled_payments_amount_fkey";
+
+-- Drop foreign key constraints in accounts table
+ALTER TABLE "accounts" DROP CONSTRAINT IF EXISTS "accounts_user_id_fkey";
+
+-- Drop tables
+DROP TABLE IF EXISTS "users";
+DROP TABLE IF EXISTS "accounts";
+DROP TABLE IF EXISTS "sessions";
+DROP TABLE IF EXISTS "transactions";
+DROP TABLE IF EXISTS "verifications";
+DROP TABLE IF EXISTS "payment_pins";
+DROP TABLE IF EXISTS "scheduled_payments";
---
+++ migration/000001_init.up.sql
@@ -0,0 +1,91 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE "users" (
+  "id" uuid DEFAULT uuid_generate_v4(),
+  "first_name" varchar,
+  "last_name" varchar,
+  "email" varchar NOT NULL,
+  "is_verified" boolean NOT NULL DEFAULT false,
+  "password" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT now(),
+  "updated_at" timestamptz NOT NULL DEFAULT now(),
+  "is_suspended" boolean,
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "accounts" (
+  "id" uuid DEFAULT uuid_generate_v4(),
+  "name" varchar,
+  "number" varchar,
+  "user_id" uuid,
+  "balance" int,
+  "created_at" timestamptz NOT NULL DEFAULT now(),
+  "updated_at" timestamptz NOT NULL DEFAULT now(),
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "sessions" (
+  "id" uuid DEFAULT uuid_generate_v4(),
+  "refresh_token" varchar,
+  "is_blocked" boolean,
+  "user_id" uuid,
+  "user_agent" varchar,
+  "client_ip" varchar,
+  "created_at" timestamptz NOT NULL DEFAULT now(),
+  "updated_at" timestamptz NOT NULL DEFAULT now(),
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "transactions" (
+  "id" uuid DEFAULT uuid_generate_v4(),
+  "type" varchar,
+  "status" varchar,
+  "user_id" uuid,
+  "amount" int,
+  "created_at" timestamptz NOT NULL DEFAULT now(),
+  "updated_at" timestamptz NOT NULL DEFAULT now(),
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "verifications" (
+  "id" uuid DEFAULT uuid_generate_v4(),
+  "secret_code" varchar,
+  "is_used" boolean,
+  "user_id" uuid,
+  "email" varchar,
+  "created_at" timestamptz NOT NULL DEFAULT now(),
+  "updated_at" timestamptz NOT NULL DEFAULT now(),
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "payment_pins" (
+  "id" uuid DEFAULT uuid_generate_v4(),
+  "pin" varchar,
+  "user_id" uuid,
+  "created_at" timestamptz NOT NULL DEFAULT now(),
+  "updated_at" timestamptz NOT NULL DEFAULT now(),
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "scheduled_payments" (
+  "id" uuid DEFAULT uuid_generate_v4(),
+  "user_id" uuid,
+  "amount" uuid,
+  "paydate" timestamptz,
+  "is_completed" boolean DEFAULT false,
+  "created_at" timestamptz NOT NULL DEFAULT now(),
+  "updated_at" timestamptz NOT NULL DEFAULT now(),
+  PRIMARY KEY ("id")
+);
+
+ALTER TABLE "accounts" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "sessions" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "transactions" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "verifications" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "payment_pins" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "scheduled_payments" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
