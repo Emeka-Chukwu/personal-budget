c8e05552f3030813a3c2a08d40b39a1c8284a781
emeka-chukwu <emekapascal11@gmail.com>
2023-12-26 20:59:03 +0100

    handler and routes

---
+++ schedule/v1/handler.go
@@ -0,0 +1,73 @@
+package schedule_payment_v1
+
+import (
+	"net/http"
+	schedule_payment_model "personal-budget/schedule/model"
+	schedule_payment_usecase "personal-budget/schedule/usecase"
+	"personal-budget/shared"
+	"personal-budget/util"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type ScheduledPaymentHandler interface {
+	CreatePlan(*gin.Context)
+	FetchPlan(*gin.Context)
+	ListPlan(*gin.Context)
+	CreatePlanTx(*gin.Context)
+}
+
+type scheduledPaymentHandler struct {
+	usecase schedule_payment_usecase.ScheduledPaymentsUsecase
+}
+
+// CreatePlan implements ScheduledPaymentHandler.
+func (p *scheduledPaymentHandler) CreatePlan(c *gin.Context) {
+	panic("unimplemented")
+
+}
+
+// CreatePlanTx implements ScheduledPaymentHandler.
+func (p *scheduledPaymentHandler) CreatePlanTx(c *gin.Context) {
+	payload := util.GetBody[schedule_payment_model.SchedulePayment](c)
+	authPayload := shared.GetAuthsPayload(c)
+	payload.UserId = authPayload.UserId
+	resp, err := p.usecase.CreatePlanTx(payload)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{"data": resp})
+}
+
+// FetchPlan implements ScheduledPaymentHandler.
+func (p *scheduledPaymentHandler) FetchPlan(c *gin.Context) {
+	params := util.GetUrlParams[ParamID](c)
+	resp, err := p.usecase.FetchPlan(params.ID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{"data": resp})
+}
+
+// ListPlan implements ScheduledPaymentHandler.
+func (p *scheduledPaymentHandler) ListPlan(c *gin.Context) {
+	authPayload := shared.GetAuthsPayload(c)
+	userID := authPayload.UserId
+	resp, err := p.usecase.ListPlan(userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{"data": resp})
+}
+
+func NewScheduledPaymentHandler(usecase schedule_payment_usecase.ScheduledPaymentsUsecase) ScheduledPaymentHandler {
+	return &scheduledPaymentHandler{usecase: usecase}
+}
+
+type ParamID struct {
+	ID uuid.UUID `uri:"id" binding:"required"`
+}
---
+++ schedule/v1/route.go
@@ -0,0 +1,17 @@
+package schedule_payment_v1
+
+import (
+	"personal-budget/middleware"
+	schedule_payment_model "personal-budget/schedule/model"
+	schedule_payment_usecase "personal-budget/schedule/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewUserRoutes(router *gin.RouterGroup, usecase schedule_payment_usecase.ScheduledPaymentsUsecase) {
+	planHandler := NewScheduledPaymentHandler(usecase)
+	route := router.Group("/plans")
+	route.POST("/create", middleware.ValidatorMiddleware[schedule_payment_model.SchedulePayment], planHandler.CreatePlanTx)
+	route.GET("/fetch/:id", planHandler.FetchPlan)
+	route.GET("/fetch/list", planHandler.ListPlan)
+}
